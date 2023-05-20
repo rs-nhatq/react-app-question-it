fa5820d45c9108890316b0b3ee62444b31945d4f
nhatq2000 <106722294+nhatq2000@users.noreply.github.com>
2023-05-20 23:35:19 +0700

    Define component

---
+++ src/views/Example/MyComponent.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  // }
+  render() {
+    return (
+      <div>
+        My Component
+      </div>
+    );
+  }
+}
+
+MyComponent.propTypes = {
+
+};
+
+export default MyComponent;
